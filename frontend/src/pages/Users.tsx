fd640d2c015e95ac3974040a04bd57036da63cae
Dinu Rusu <dinu@codingshadows.com>
2024-09-03 17:02:49 +0300

    user table

--- frontend/src/pages/Users.tsx
+++ frontend/src/pages/Users.tsx
@@ -1,36 +1,50 @@
 import {useEffect, useState} from 'react';
 import {Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table.tsx";
-
+import {IUser} from "@/bemodel/Api.ts";
+import axios from 'axios';
 
 function Users() {
-
+    const [users, setUsers] = useState<IUser[]>([]);
 
     useEffect(() => {
-        // fetch a list of users
+        axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/v1/users`, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => {
+                setUsers(response.data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
     }, []);
 
     return (
         <>
             <Table>
-                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableCaption>A list of users.</TableCaption>
                 <TableHeader>
                     <TableRow>
-                        <TableHead className="w-[100px]">Invoice</TableHead>
-                        <TableHead>Status</TableHead>
-                        <TableHead>Method</TableHead>
-                        <TableHead className="text-right">Amount</TableHead>
+                        <TableHead className="w-[100px]">ID</TableHead>
+                        <TableHead>Keycloak ID</TableHead>
+                        <TableHead>Created Date</TableHead>
+                        <TableHead>Deleted</TableHead>
+                        <TableHead>Document Count</TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    <TableRow>
-                        <TableCell className="font-medium">INV001</TableCell>
-                        <TableCell>Paid</TableCell>
-                        <TableCell>Credit Card</TableCell>
-                        <TableCell className="text-right">$250.00</TableCell>
-                    </TableRow>
+                    {users.map((user) => (
+                        <TableRow key={user.id}>
+                            <TableCell className="font-medium">{user.id}</TableCell>
+                            <TableCell>{user.keycloakId}</TableCell>
+                            <TableCell>{user.createdDate}</TableCell>
+                            <TableCell>{user.deleted ? 'Yes' : 'No'}</TableCell>
+                            <TableCell>{user.documents?.length ?? 0}</TableCell>
+                        </TableRow>
+                    ))}
                 </TableBody>
             </Table>
-
         </>
     );
 }
