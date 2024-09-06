7e1275e0d79da9d470c32b39212c9d1d865937ed
Rusu Dinu <dinu@codingshadows.com>
2024-09-06 11:44:53 +0300

    updated boot script

--- hyperledger/server-boot.sh
+++ hyperledger/server-boot.sh
@@ -5,8 +5,6 @@ cd network
 ./network.sh down
 ./network.sh up createChannel
 sleep 2
-./network.sh deployCC -ccn prescription -ccp ../contracts/prescription -ccl typescript
-sleep 2
-./network.sh deployCC -ccn vitaldata -ccp ../contracts/vital-data -ccl typescript
+./network.sh deployCC -ccn registry -ccp ../contracts/registry -ccl typescript
 sleep 2
 echo "[BOOT] ---CHAINCODE DEPLOYED---"
