fa627b1c8835cfff1fce1b693441c0c2f4d54133
Rusu Dinu <dinustefan89.ds@gmail.com>
2025-05-02 20:56:07 +0300

    Simplify HashService by removing unused cryptographic code
    Eliminated redundant RSA encryption/decryption logic and related dependencies to streamline the HashService class. Focused the service solely on hashing operations to reduce complexity and enhance maintainability.

--- backend/src/main/java/com/rusudinu/backend/hash/HashService.java
+++ backend/src/main/java/com/rusudinu/backend/hash/HashService.java
@@ -1,33 +1,16 @@
 package com.rusudinu.backend.hash;
 
-import com.rusudinu.backend.document.Document;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
-import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
-import org.bouncycastle.asn1.x509.DigestInfo;
-import org.bouncycastle.operator.DefaultDigestAlgorithmIdentifierFinder;
-import org.bouncycastle.operator.DigestAlgorithmIdentifierFinder;
 
 import org.springframework.stereotype.Service;
-
-import javax.crypto.Cipher;
-
-import java.security.*;
-import java.util.Arrays;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class HashService {
-	private final PublicKey ministryPublicKey;
-	private final PrivateKey ministryPrivateKey;
-	private final MessageDigest messageDigest;
-
 	public String shaHash(String data) {
 		if (data == null || data.isEmpty()) {
 			log.warn("Data is null or empty, returning empty hash");
@@ -39,28 +22,5 @@ public class HashService {
 	@SneakyThrows
 	public String hashDocument(byte[] document) {
 		return DigestUtils.sha256Hex(document);
-	}
-
-	@SneakyThrows
-	private byte[] createDigestInfo(byte[] data) {
-		byte[] messageHash = messageDigest.digest(data);
-		DigestAlgorithmIdentifierFinder hashAlgorithmFinder = new DefaultDigestAlgorithmIdentifierFinder();
-		AlgorithmIdentifier hashingAlgorithmIdentifier = hashAlgorithmFinder.find("SHA-256");
-		DigestInfo digestInfo = new DigestInfo(hashingAlgorithmIdentifier, messageHash);
-		return digestInfo.getEncoded();
-	}
-
-	@SneakyThrows
-	private byte[] encryptWithRSA(byte[] dataToEncrypt) {
-		Cipher cipher = Cipher.getInstance("RSA");
-		cipher.init(Cipher.ENCRYPT_MODE, ministryPrivateKey);
-		return cipher.doFinal(dataToEncrypt);
-	}
-
-	@SneakyThrows
-	private byte[] decryptWithRSA(byte[] dataToDecrypt) {
-		Cipher cipher = Cipher.getInstance("RSA");
-		cipher.init(Cipher.DECRYPT_MODE, ministryPublicKey);
-		return cipher.doFinal(dataToDecrypt);
 	}
 }
